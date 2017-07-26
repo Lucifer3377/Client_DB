6cda9dbc2e540eba7e916128eb3af43d0fd2220d
Sai_Kameswari <Sai_Kameswari@Kammi>
2017-07-26 16:55:42 +0530

    Kept Delete and status in Switch in Team Manage Process

---
+++ src/java/com/nvmate/Helper/CollectionCompare.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nvmate.Helper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Sai_Kameswari
+ */
+public class CollectionCompare {
+    
+    
+    public static void addToMap(int sno, String heading, String value)
+    {
+        
+    }
+    
+    public static Map createMap()
+    {
+        Map<Integer, String> map = new HashMap<>();
+        return map;
+    }
+        
+    
+}
